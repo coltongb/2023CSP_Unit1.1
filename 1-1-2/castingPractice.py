6d56064a556c709f871377f2c115e408dd3656f5
Colton <colton.bates@Vtsd.com>
2023-09-19 10:06:59 -0400

    Test file

---
+++ 1-1-2/castingPractice.py
@@ -0,0 +1,13 @@
+gart = 1.2
+shmuggit = 65
+blunger = "gubba"
+print("Initial Values:")
+print("gart:", gart)
+print("shmuggit:", shmuggit)
+print("blunger:", blunger)
+
+shmuggit = chr(shmuggit)
+
+print("Ending Values:")
+print("gart:", gart)
+print("shmuggit:", shmuggit)
