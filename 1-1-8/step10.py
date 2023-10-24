344cbbd1ac01c5b050dae87fd3ffc475b2fd5981
Colton <colton.bates@Vtsd.com>
2023-10-24 08:40:19 -0400

    Test file

---
+++ 1-1-8/step10.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number -> "))
+if (num >= 90):
+  print("A")
+elif (num >= 80):
+  print("B")
+elif (num >= 70):
+  print("C")
+elif (num >= 60):
+  print("D")
---
+++ 1-1-8/step12.py
@@ -0,0 +1,8 @@
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+my_courses = ["English", "Math", "CS"]
+
+for d in days_of_week:
+  print(d)
+
+  for c in my_courses:
+    print("  " + c)
